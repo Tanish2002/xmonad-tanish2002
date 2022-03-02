37f3de6dc27c4ac237a79bd64d0237d432daec72
Tanish2002 <tanishkhare@gmail.com>
2022-03-02 16:54:43 +0530

    simplify the flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,10 @@
-# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 {
-  description = "A flake for installing my xmonad build";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
+  description = "A flake for installing this picom build";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  
+  outputs = { self, nixpkgs, ... }: {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      pkgs.haskellPackages.callCabal2nix "xmonad-tanish2002" self { };
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "xmonad-tanish2002";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
-
-        defaultPackage = self.packages.${system}.${packageName};
-      });
 }
